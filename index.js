4ee35f601859244149b436e6b61a7ef2765c4423
ascotttoney <ascotttoney@gmail.com>
2019-06-11 08:45:33 -0400

    Done.

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ class Polygon {
 
 class Triangle extends Polygon {
   get isValid() {
-    if ( !Array.isArray( this.array ) ) return;
-    if ( this.count !== 3 ) return;
-    let side1 = this.array[ 0 ]
-    let side2 = this.array[ 1 ]
-    let side3 = this.array[ 2 ]
-    return ( ( side1 === side2 ) && ( side1 === side3 ) && ( side2 === side3 ) )
+    if (!Array.isArray(this.array)) return;
+    if (this.count !== 3) return;
+    let side1 = this.array[0]
+    let side2 = this.array[1]
+    let side3 = this.array[2]
+    return ((side1=== side2) && (side1 === side3) && (side2 === side3))
   }
 }
 
